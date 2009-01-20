df136a2bacb12fa9504a8345ed86c29d812a5cc1
cornel.alders <cornel.alders@c8f4e16c-bd43-11dd-9405-3d0920048215>
2009-01-20 09:43:35 +0000

    Latest test version.
    git-svn-id: https://openpacman.googlecode.com/svn/trunk@34 c8f4e16c-bd43-11dd-9405-3d0920048215

--- OpenPacMan/OpenPacMan/Game1.cs
+++ OpenPacMan/OpenPacMan/Game1.cs
@@ -8,23 +8,39 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
-using Microsoft.Xna.Framework.Net;
 using Microsoft.Xna.Framework.Storage;
 
 namespace OpenPacMan
 {
     /// <summary>
-    /// This is the main type for your game
+    /// Free pacman clone created by students in the netherlands
     /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        KeyboardState keyboard = Keyboard.GetState();
+        Texture2D background;
+        Player pacman;
+        Song gamemusic;
+        Song gamemusic2;
+        AudioEngine audioEngine;
+        WaveBank waveBank;
+        SoundBank soundBank;
+        Cue effectSound;
+        SpriteFont font;
+
+        KeyboardState oldState;
+
+        // var for start music
+        int playstart = 0;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
+
             Content.RootDirectory = "Content";
+            pacman = new Player(this);
         }
 
         /// <summary>
@@ -36,7 +52,8 @@ namespace OpenPacMan
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            this.Components.Add(pacman);
+            oldState = Keyboard.GetState();
             base.Initialize();
         }
 
@@ -48,10 +65,22 @@ namespace OpenPacMan
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            this.Services.AddService(typeof(SpriteBatch), spriteBatch);
             // TODO: use this.Content to load your game content here
-        }
+            graphics.PreferredBackBufferWidth = 600;
+            graphics.PreferredBackBufferHeight = 350;
+            graphics.ApplyChanges();
+            background = this.Content.Load<Texture2D>(@"Images\background");
+            font = this.Content.Load<SpriteFont>(@"font");
+            gamemusic = this.Content.Load<Song>(@"Audio\Music\background");
+            gamemusic2 = this.Content.Load<Song>(@"Audio\Music\background2");
+            
+            audioEngine = new AudioEngine(@"Content\Audio\sounds.xgs");
+            waveBank = new WaveBank(audioEngine, @"Content\Audio\WBank.xwb");
+            soundBank = new SoundBank(audioEngine, @"Content\Audio\SBank.xsb");
 
+        }
+        
         /// <summary>
         /// UnloadContent will be called once per game and is the place to unload
         /// all content.
@@ -71,23 +100,92 @@ namespace OpenPacMan
             // Allows the game to exit
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
+            keyboard = Keyboard.GetState();
+            if (keyboard.IsKeyDown(Keys.Escape))
+                this.Exit();
+
+            KeyboardState newState = Keyboard.GetState();
+
+            // Get keys pressed now that weren't pressed before
+            var newPressedKeys = from k in newState.GetPressedKeys()
+                                 where !(oldState.GetPressedKeys().Contains(k))
+                                 select k;
+
+            if(newPressedKeys.Contains(Keys.A))
+            {
+                soundBank.PlayCue("fruiteat");
+            }
+
+            this.pacman.pressedKeys(Keyboard.GetState(), this.pacman.DestRect);
 
             // TODO: Add your update logic here
+            if (keyboard.IsKeyDown(Keys.F1))
+            {
+                MediaPlayer.Play(gamemusic);
+            }
+            if (keyboard.IsKeyDown(Keys.F2))
+            {
+                MediaPlayer.Play(gamemusic2);
+            }
+            if (keyboard.IsKeyDown(Keys.F3))
+            {
+                MediaPlayer.Stop();
+            }
 
+            if (keyboard.IsKeyDown(Keys.S))
+            {
+                soundBank.PlayCue("fruiteat");
+            }
+            if (keyboard.IsKeyDown(Keys.D))
+            {
+                soundBank.PlayCue("ghosteaten");
+            }
+            if (keyboard.IsKeyDown(Keys.F))
+            {
+                soundBank.PlayCue("interm");
+            }
+            if (keyboard.IsKeyDown(Keys.G))
+            {
+                soundBank.PlayCue("killed");
+            }
+            if (keyboard.IsKeyDown(Keys.H))
+            {
+                soundBank.PlayCue("pacchomp");
+            }
+            if (keyboard.IsKeyDown(Keys.J))
+            {
+                soundBank.PlayCue("start");
+            }
             base.Update(gameTime);
         }
-
+        
         /// <summary>
         /// This is called when the game should draw itself.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            BeginDraw();
+            GraphicsDevice.Clear(Color.Black);
+            spriteBatch.Begin(SpriteBlendMode.AlphaBlend);
+            spriteBatch.Draw(background,new Vector2(0,0), Color.White);
+            Texture2D logo = Content.Load<Texture2D>(@"Images\Sprites\logo");
+            spriteBatch.Draw(logo, new Vector2(300, 10),Color.White);
+            pacman.Draw(gameTime, spriteBatch);
+            spriteBatch.DrawString(font,"Score: ", new Vector2(10, 30), Color.Red);
+            spriteBatch.End();
             // TODO: Add your drawing code here
-
+            
             base.Draw(gameTime);
+            EndDraw();
+            if (playstart == 0)
+            {
+                soundBank.GetCue("start").Play();
+                System.Threading.Thread.Sleep(4411);
+                //soundBank.GetCue("pacchomp").Play();
+                playstart = 1;
+            }
+            
         }
     }
 }
---
+++ OpenPacMan/OpenPacMan/Player.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace OpenPacMan
+{
+
+    public class Player : Microsoft.Xna.Framework.DrawableGameComponent
+    {
+         KeyboardState keyboard = Keyboard.GetState();
+        ///sprite data
+        protected Texture2D spritesheet = null;
+        protected Vector2 spritePosition;
+        
+        protected int moveDirection;
+        int i = 0;
+        float Timer = 0f;
+        protected string key;
+        protected int killed = 0;
+
+        /// animation logic
+        float Interval = 1000f / 10f;
+        int FrameCount = 3;
+        int CurrentFrameX = 0;
+        int CurrentFrameY = 0;
+        int SpriteWidth = 14;
+        int SpriteHeight = 14;
+        public Rectangle SourceRect;
+        public Rectangle DestRect = new Rectangle(20, 320, 14, 14);
+
+        private Game game;
+
+        public Player(Game game)
+            : base(game)
+        {
+            this.game = game;
+            // TODO: Construct any child components here
+        }
+
+        public override void Initialize()
+        {
+            // TODO: Add your initialization code here
+            base.Initialize();
+            this.spritesheet = game.Content.Load<Texture2D>(@"Images\Sprites\pacman");
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update code here
+
+            Timer += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+            base.Update(gameTime);
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            // TODO: Add your drawing code here
+            SourceRect = new Rectangle(CurrentFrameX * SpriteWidth, 0, SpriteWidth, SpriteHeight);
+            spriteBatch.Draw(spritesheet, DestRect, SourceRect, Color.White, 0, new Vector2(0,0), SpriteEffects.None, 0);
+            base.Draw(gameTime);
+        }
+
+        public void pressedKeys(KeyboardState k_State, Rectangle s_Position)
+        {
+            keyboard = (KeyboardState)k_State;
+            DestRect = (Rectangle)s_Position;
+            int Xpos = DestRect.X;
+            int Ypos = DestRect.Y;
+            key = keyboard.GetPressedKeys().ToString();
+            Keys[] keysPressed = keyboard.GetPressedKeys();
+
+            if (keyboard.IsKeyDown(Keys.Down) && keyboard.IsKeyDown(Keys.Right))
+            {
+                if (keyboard.IsKeyDown(Keys.Down))
+                {
+                    Ypos += 2;
+                    DestRect = new Rectangle(Xpos, Ypos, 14, 14);
+                }
+                else if (keyboard.IsKeyDown(Keys.Right))
+                {
+                    Xpos += 2;
+                    DestRect = new Rectangle(Xpos, Ypos, 14, 14);
+                } 
+                this.moveAni();
+            }
+            else if (keyboard.IsKeyDown(Keys.Down) && keyboard.IsKeyDown(Keys.Left))
+            {
+                if (keyboard.IsKeyDown(Keys.Down))
+                {
+                    Ypos += 2;
+                    DestRect = new Rectangle(Xpos, Ypos, 14, 14);
+                }
+                else if (keyboard.IsKeyDown(Keys.Left))
+                {
+                    Xpos += -2;
+                    DestRect = new Rectangle(Xpos, Ypos, 14, 14);
+                } 
+                this.moveAni();
+            }
+            else if (keyboard.IsKeyDown(Keys.Up) && keyboard.IsKeyDown(Keys.Right))
+            {
+                if (keyboard.IsKeyDown(Keys.Up))
+                {
+                    Ypos += -2;
+                    DestRect = new Rectangle(Xpos, Ypos, 14, 14);
+                }
+                else if (keyboard.IsKeyDown(Keys.Right))
+                {
+                    Xpos += 2;
+                    DestRect = new Rectangle(Xpos, Ypos, 14, 14);
+                } 
+                
+                this.moveAni();
+            }
+            else if (keyboard.IsKeyDown(Keys.Up) && keyboard.IsKeyDown(Keys.Left))
+            {
+                if (keysPressed[0] == Keys.Up)
+                {
+                    Ypos += -2;
+                    DestRect = new Rectangle(Xpos, Ypos, 14, 14);
+                }
+                else if (keysPressed[0] == Keys.Left)
+                {
+                    Xpos += -2;
+                    DestRect = new Rectangle(Xpos, Ypos, 14, 14);
+                }
+                this.moveAni();
+            }
+            else
+            {
+                if (keyboard.IsKeyDown(Keys.Up))
+                {
+                    this.rotate(0);
+                    Ypos += -2;
+                    DestRect = new Rectangle(Xpos, Ypos, 14, 14);
+                    this.moveAni();
+                }
+                if (keyboard.IsKeyDown(Keys.Right))
+                {
+                    this.rotate(1);
+                    Xpos += 2;
+                    DestRect = new Rectangle(Xpos, Ypos, 14, 14);
+                    this.moveAni();
+                }
+                if (keyboard.IsKeyDown(Keys.Down))
+                {
+                    this.rotate(2);
+                    Ypos += 2;
+                    DestRect = new Rectangle(Xpos, Ypos, 14, 14);
+                    this.moveAni();
+                }
+                if (keyboard.IsKeyDown(Keys.Left))
+                {
+                    this.rotate(3);
+                    Xpos += -2;
+                    DestRect = new Rectangle(Xpos, Ypos, 14, 14);
+                    this.moveAni();
+                }
+            }
+        }
+
+        public void moveAni()
+        {
+
+            if (Timer > Interval)
+            {
+                CurrentFrameX++;
+                if (CurrentFrameX > FrameCount - 1)
+                {
+                    CurrentFrameX = 0;
+
+                }
+                Timer = 0f;
+            }
+            SourceRect = new Rectangle(CurrentFrameX * SpriteWidth, CurrentFrameY, SpriteWidth, SpriteHeight);
+        }
+
+        public void rotate(int direction)
+        {
+            this.moveDirection = direction;
+            if (this.moveDirection == 0)
+            {
+                ///spritesheet xpos +
+            }
+            else if (this.moveDirection == 1)
+            {
+                
+            }
+            else if (this.moveDirection == 2)
+            {
+                
+            }
+            else if (this.moveDirection == 3)
+            {
+                
+            }
+
+        }
+        public void move()
+        {
+
+        }
+
+    }
+}
