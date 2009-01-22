f23ef1350397e72004b96f0cc502f676e67bf4b2
Cornel.alders <Cornel.alders@c8f4e16c-bd43-11dd-9405-3d0920048215>
2009-01-22 20:28:26 +0000

    empty pill class.
    git-svn-id: https://openpacman.googlecode.com/svn/trunk@52 c8f4e16c-bd43-11dd-9405-3d0920048215

---
+++ OpenPacMan/OpenPacMan/Pill.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+
+namespace OpenPacMan
+{
+    public class Pill : Microsoft.Xna.Framework.DrawableGameComponent
+    {
+        public Pill(Game game)
+            : base(game)
+        {
+            
+        }
+
+        public override void Initialize()
+        {
+
+            base.Initialize();
+        }
+        protected override void LoadContent()
+        {
+
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+            base.Update(gameTime);
+        }
+    }
+}
